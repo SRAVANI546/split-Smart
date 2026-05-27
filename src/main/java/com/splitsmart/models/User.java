dfeb9bc00d54bc3daa3068003ba9acd2f10b84ad
SRAVANI546 <144050925+SRAVANI546@users.noreply.github.com>
2026-05-27 07:02:54 +0530

    Add User entity model

---
+++ src/main/java/com/splitsmart/models/User.java
@@ -0,0 +1,20 @@
+package com.splitsmart.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    
+    private String passwordHash;
+}
